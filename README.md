f08892bf7cb9b2979918974720f11bfb7f767027
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-09-26 19:21:08 -0300

    Tutorial completo

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://www.youtube.com/watch?v=T1intZyhXDU
 ## To run
 $ virtualenv venv <br>
 $ pip install -r requirements.txt
+$ . /venv/Scripts/activate
+$ python manage.py runserver
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from unicodedata import name
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Project, Task
+from .forms import CreateNewProject, CreateNewTask
 
 # Create your views here.
+
+
 def index(request):
     title = "Welcome to Django Course"
     return render(request, "index.html", {
         "title": title
     })
+
 
 def about(request):
     username = "Fazt"
@@ -20,15 +25,45 @@ def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
 
 
-
 def projects(request):
     projects = Project.objects.all()
-    return render(request, "projects.html", {
+    return render(request, "projects/projects.html", {
         "projects": projects
     })
 
+
 def tasks(request):
     tasks = Task.objects.all()
-    return render(request, "tasks.html", {
+    return render(request, "tasks/tasks.html", {
+        "tasks": tasks
+    })
+
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "tasks/create_task.html", {
+            "form": CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('tasks')
+
+
+def create_project(request):
+    if request.method == "GET":
+        return render(request, "projects/create_project.html", {
+            "form": CreateNewProject
+        })
+    else:
+        Project.objects.create(name=request.POST["name"])
+        
+        return redirect("projects")
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, "projects/detail.html", {
+        "project": project,
         "tasks": tasks
     })
